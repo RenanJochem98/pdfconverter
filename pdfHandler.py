675ded59c77d2baf9c4e1d16ea80f680370c275f
DEVDIMAS03 <renanjochem98@gmail.com>
2019-07-23 14:02:29 -0300

    Criando manipulador de pdf

---
+++ pdfHandler.py
@@ -0,0 +1,35 @@
+from PyPDF2 import PdfFileReader
+
+class PdfHandler(object):
+
+    def __init__(self, pdfFile):
+        self.openFile(pdfFile)
+
+    def openFile(self, pdfFile):
+        self.pdfObj = open(pdfFile, 'rb')
+        self.pdf = PdfFileReader(self.pdfObj)
+
+        self.info = self.pdf.getDocumentInfo()
+        self.number_of_pages = self.pdf.getNumPages()
+
+    def closePdf(self):
+        self.pdfObj.close()
+
+    def getPdfMetadata(self):
+        metadata = {}
+
+        metadata["number_of_pages"] = self.number_of_pages
+        metadata["info"] = {}
+        metadata["info"]["default"] = self.info
+        metadata["info"]['producer'] = self.info.producer
+        metadata["info"]['author'] = self.info.author
+        metadata["info"]['creator'] = self.info.creator
+
+        return metadata
+
+    def getText(self):
+        text = ""
+        for pageNumber in range(1, self.number_of_pages):
+            text = text + " " + self.pdf.getPage(pageNumber).extractText()
+
+        return text
