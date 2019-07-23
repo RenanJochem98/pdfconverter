3c8a9dc1a1512ab48446b786e7776b04bb327075
DEVDIMAS03 <renanjochem98@gmail.com>
2019-07-23 14:03:20 -0300

    Pequenas modificações para testes

--- init.py
+++ init.py
@@ -1,3 +1,9 @@
-import PyPDF2
+from pdfHandler import PdfHandler
 
-pdfFileObj = open('teste.pdf', 'rb')
+pdf_bigger = 'pdftests/pdf_bigger.pdf'
+pdf_smaller = 'pdftests/pdf_smaller.pdf'
+pdf = PdfHandler(pdf_smaller)
+# info = pdf.getPdfMetadata()
+text = pdf.getText()
+pdf.closePdf()
+print text
